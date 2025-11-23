f4a545eaf51a8f5be0fd81fb3d8374563241ef72
Arbaaz Ahmed <arbaazahmed419@gmail.com>
2025-11-23 08:56:05 +0530

    Day3

---
+++ day3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Arbaaz Ahmed \n" << "studying in BMSIT";
+    cout << "Welcome to the programming world!";
+    cout << "Let's learn and grow together!";
+    return 0;
+}
