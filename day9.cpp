1a531667fee201d1f356938e0ac3a8f2b11fa2e4
Arbaaz Ahmed <arbaazahmed419@gmail.com>
2025-12-03 16:50:09 +0530

    variables

---
+++ day9.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a=10;
+    int b=20;
+    cout << a*b << endl;
+    return 0;
+}
+
+
+// Garbage value
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<a;
+    return 0;
+}
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    a = 100;
+    cout<<a;
+    return 0;
+}
