a58420fd6add9d4339aea942ea0c9113dc3a63ec
Arbaaz Ahmed <arbaazahmed419@gmail.com>
2025-12-04 19:20:10 +0530

    operators

---
+++ day10.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //binary operators
+    //Addition
+    cout<<(10+9)<<endl;
+    //Subration
+    cout<<(100-90)<<endl;
+    //multiplication
+    cout<<(10*5)<<endl;
+    //division
+    cout<<(10/5)<<endl;
+    //modulo
+    cout<<(500%10)<<endl;
+    // OR
+    int a;
+    int b;
+    cout<<"Enter the first number = ";
+    cin>>a;
+    cout<<"Enter the second number = ";
+    cin>>b;
+    cout<<"Addition :- "<<(a+b)<<endl;
+    cout<<"Subraction :- "<<(a-b)<<endl;
+    cout<<"Multiplication :- "<<(a*b)<<endl;
+    cout<<"Division :- "<<(a/b)<<endl;
+    cout<<"Modulo :- "<<(a%b)<<endl;
+    return 0;
+}
