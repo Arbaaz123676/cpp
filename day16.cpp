ffcc57a79cf59b813b583286b86e0f69540630a3
Arbaaz Ahmed <arbaazahmed419@gmail.com>
2025-12-10 18:20:55 +0530

    explicit convertion

---
+++ day16.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<(int)('A');
+    double pi = 3.14789654711258963574169;
+    cout<<"\n"<<(int)(pi)<<endl;
+    cout<<(float)(pi)<<endl;
+    cout<<((float)10/3)<<endl;
+    cout<<(char)('A'+2)<<endl;
+
+
+
+    return 0;
+}
