c137855e9cd8dff5777c6403c685e04ea54886fd
Arbaaz Ahmed <arbaazahmed419@gmail.com>
2025-12-11 14:58:50 +0530

    relation operators

---
+++ day17.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    int b = 20;
+    cout<<(a>b)<<endl;
+    cout<<(a<b)<<endl;
+    cout<<(a>=b)<<endl;
+    cout<<(a<=b)<<endl;
+    int x = 10;
+    int y = 10;
+    cout<<(x=y)<<endl;
+    cout<<(x!=y)<<endl;
+
+
+    return 0;
+}
