f271a2355f1c2c885528214ca5900f48272a186a
Arbaaz Ahmed <arbaazahmed419@gmail.com>
2025-12-18 17:46:54 +0530

    Add p5.cpp

---
+++ practice/p5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#define pi 3.14
+int main(){
+    float R;
+    cout<<"Enter the radius of the circle = ";
+    cin>>R;
+    float A;
+    A = pi*R*R;
+    cout<<"The area of the circle is = "<<A;
+
+    return 0;
+}
