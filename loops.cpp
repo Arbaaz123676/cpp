006157f2a70a6f51f53bdf62d695579a4a99831b
Arbaaz Ahmed <arbaazahmed419@gmail.com>
2025-12-27 18:19:45 +0530

    Add loops.cpp (loop examples)

---
+++ loops.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a;
+for (a=1; a<=100000000; a++) {
+    cout<<"Apna college "<<endl;
+}
+cout<<"The last number is = "<<a;
+
+
+return 0;
+}
