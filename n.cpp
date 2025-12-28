704e7fcd1825390aa55e369330e791da2bca71eb
Arbaaz Ahmed <arbaazahmed419@gmail.com>
2025-12-28 10:11:45 +0530

    Add n.cpp

---
+++ n.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"Enter the number till which u have to print = ";
+cin>>n;
+for(int a=0; a<=n; a++){
+
+    cout<<a<<endl;
+
+}
+
+
+
+return 0;
+}
