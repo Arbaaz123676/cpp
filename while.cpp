7f46c1c34e29b69c43ca39a464ddb116d6656918
Arbaaz Ahmed <arbaazahmed419@gmail.com>
2026-01-01 10:41:45 +0530

    Add while.cpp (while-loop example)

---
+++ while.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"Enter the num - ";
+cin>>n;
+int c = 0;
+while(c<=n){
+    cout<<"Apna college"<<endl;
+    c++;
+}
+return 0;
+}
