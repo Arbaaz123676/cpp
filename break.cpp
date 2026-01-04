e24f1a544a640b89383e4e598ef486b71d4918e6
Arbaaz Ahmed <arbaazahmed419@gmail.com>
2026-01-04 22:23:08 +0530

    Add break.cpp (break example)

---
+++ break.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i = 1;
+    while (i>0){
+        cout<<"Apna college"<<endl;
+        if(i>=10){
+            break;
+        }
+        i++;
+    }
+    return 0;
+}
