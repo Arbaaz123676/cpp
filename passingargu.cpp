1d0d6c5b0c0cc70bc2adaf394f18907f6f7ed30d
Arbaaz Ahmed <arbaazahmed419@gmail.com>
2026-01-23 11:55:18 +0530

    Add passingargu.cpp

---
+++ passingargu.cpp
@@ -0,0 +1,13 @@
+//pass by value
+#include <iostream>
+using namespace std;
+void changea(int a){
+    a = 20;
+    cout<<a<<endl;
+}
+int main() {
+    int a = 10;
+    cout<<a<<endl;
+    changea(a);
+    cout<<a<<endl;
+} 
