34b9537af1c9f8777fb7a69e792ffdff084db3a4
Arbaaz Ahmed <arbaazahmed419@gmail.com>
2026-01-31 20:47:44 +0530

    Add funbasics.cpp

---
+++ funbasics.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+void sayhello() {
+    cout<<"Iam doing the work now! \n";
+    cout<<"Hello world :)\n";
+}
+int main(){
+    sayhello();
+    return 0;
+}
